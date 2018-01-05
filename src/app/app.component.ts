3fc9f86ae81872f64ab8af375c072d467d5cdffb
AlexVerzeaDeloitte <averzea@deloitte.ca>
2018-01-05 11:30:08 -0500

    Firebase start underway.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyB-vAoSOXGPGqW_hZBNtYb3Qbtqc05FynY",
+      authDomain: "angular-firebase-app-f846b.firebaseapp.com",
+      databaseURL: "https://angular-firebase-app-f846b.firebaseio.com",
+      projectId: "angular-firebase-app-f846b",
+      storageBucket: "angular-firebase-app-f846b.appspot.com",
+      messagingSenderId: "1033756615151"
+    };
+    firebase.initializeApp(config);
+
+    var root = firebase.database().ref();
+    root.on('value', function (snap) {
+      console.log(snap.key, snap.val());
+    });
+
+  }
 }
